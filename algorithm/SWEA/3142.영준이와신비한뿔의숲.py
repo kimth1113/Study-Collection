c430283fc5e04c4f883134908172f20fd3b181f8
kimth1113 <kimth1113@gmail.com>
2021-06-21 01:13:43 +0900

    add 3142 problom

---
+++ algorithm/SWEA/3142.영준이와신비한뿔의숲.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('3142.txt', 'r')
+
+def devAnimal(n, m):
+    unicorn = 0
+    twinHorn = 0
+
+    if n == m:
+        unicorn = m
+        return [unicorn, twinHorn]
+
+    while n > m: 
+        n -= 2
+        m -= 1
+        twinHorn += 1
+
+    unicorn = n
+
+    return [unicorn, twinHorn]
+
+TC = int(input())
+for tc in range(1, TC+1):
+    N, M = list(map(int, input().split()))
+    arr = devAnimal(N, M)
+
+    print(f'#{tc} {arr[0]} {arr[1]}')

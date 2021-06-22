2dac4827faa8974bb8dc56315e4a2e5d0fbf8f39
kimth1113 <kimth1113@gmail.com>
2021-06-22 23:07:11 +0900

    add watermelon

---
+++ algorithm/PROGRAMMERS/watermelon.js
@@ -0,0 +1,17 @@
+// 수박수박수박수박수박수? level1
+
+function solution(n, f1, f2) {
+    let word = ''
+    
+    let cnt = 1;
+    while (n > 0) {
+        if (cnt % 2 === 1){
+            word += '수'
+        } else {
+            word += '박'
+        }
+        n--;   
+        cnt++;
+    }
+    return word;
+}
